22174ff8f9a3d0acec56ca68c8f316199ec97324
Ldpe2G <liangdepeng@mail.com>
2019-10-04 08:56:01 +0800

    feat: update convert.sh script to generate epub files

--- convert.sh
+++ convert.sh
@@ -1,10 +1,29 @@
 PROJECT_ROOT=$(cd "$(dirname $0)"; pwd)
 
 SCALA_PRODECT_DIR=${PROJECT_ROOT}/scala/spec
-RESULT_DIR=pdf
-	
+
+# generate pdf files
+PDF_RESULT_DIR=pdf
+
+if [ ! -d $PDF_RESULT_DIR ] ; then
+  mkdir -p $PDF_RESULT_DIR
+fi
+
 ls ${SCALA_PRODECT_DIR} \
 	| grep ".md" \
 	| grep '^[0-9]\|index' \
-	| awk -F ".md" -v scala_path="$SCALA_PRODECT_DIR" -v result_path="$RESULT_DIR" '{print " pandoc " scala_path "/"  $0 " -s -o " result_path "/" $1 ".pdf --pdf-engine=xelatex --standalone"}' \
+	| awk -F ".md" -v scala_path="$SCALA_PRODECT_DIR" -v result_path="$PDF_RESULT_DIR" '{print " pandoc " scala_path "/"  $0 " -s -o " result_path "/" $1 ".pdf --pdf-engine=xelatex --standalone"}' \
+	| sh
+
+# generate epub files
+EPUB_RESULT_DIR=epub	
+
+if [ ! -d $EPUB_RESULT_DIR ] ; then
+  mkdir -p $EPUB_RESULT_DIR
+fi
+
+ls ${SCALA_PRODECT_DIR} \
+	| grep ".md" \
+	| grep '^[0-9]\|index' \
+	| awk -F ".md" -v scala_path="$SCALA_PRODECT_DIR" -v result_path="$EPUB_RESULT_DIR" '{print " pandoc " scala_path "/"  $0 " -s -o " result_path "/" $1 ".epub "}' \
 	| sh
